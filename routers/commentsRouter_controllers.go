404463b18c3d090d91309177f23e834fa66535d5
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-05-24 15:02:29 -0500

    se configura endpoint para obtener convenios #16

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,31 +7,22 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:ConvenioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:ConvenioController"],
-        beego.ControllerComments{
-            Method: "GetConvenio",
-            Router: `/GetConvenio`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:ConvenioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:ConvenioController"],
+		beego.ControllerComments{
+			Method:           "GetConvenio",
+			Router:           `/GetConvenio`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"],
-        beego.ControllerComments{
-            Method: "GetMovilidad",
-            Router: `/GetMovilidad`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"],
-        beego.ControllerComments{
-            Method: "GetRondasLiga",
-            Router: `/GetRondasLiga`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_mid/controllers:MovilidadController"],
+		beego.ControllerComments{
+			Method:           "GetMovilidad",
+			Router:           `/GetMovilidad`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
